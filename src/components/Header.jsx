a8b3394665fc458fea5bf88c10a3004d2f172b65
Aditya som <ADITYASOAM55@GMAIL.COM>
2025-11-06 11:48:45 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="text-center mt-4">
+      <h1 className="text-3xl font-bold text-blue-600">To-Do List</h1>
+      <p className="text-gray-500 mt-1">Manage your daily tasks efficiently</p>
+    </header>
+  );
+}
+
+export default Header;
